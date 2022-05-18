cabdea6a896047f7e4497d42facf1a548ae356ca
Udayashankar MS <udayashankar.ms@global-infonet.com>
2022-05-18 22:06:18 +0530

    Third Commit

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from common.utilities import Trainer
 app = Flask(__name__)
 
 
+@app.route("/", methods=['GET'])
+def hello():
+    return "Hello, World!"
+
+
 # on the terminal type: curl http://127.0.0.1:5000/
 # returns hello world when we use GET.
 # returns the data that we send when we use POST.
