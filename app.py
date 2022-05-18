385f7161ce04259447d84fbbf111cb8088d64066
Udayashankar MS <udayashankar.ms@global-infonet.com>
2022-05-18 22:21:01 +0530

    Fifth Commit

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns hello world when we use GET.
+# returns the data that we send when we use POST.
+@app.route('/extract', methods=['POST'])
+def home():
+    data = json.loads(request.data)
+    model = data["model"]
+    document = data["doc"]
+
+    trainer = Trainer()
+    entities = trainer.extract_entities(document, model)
+    filtered_entities = Trainer.convert_result(entities.ents)
+    return jsonify(filtered_entities)
