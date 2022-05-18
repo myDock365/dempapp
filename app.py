3464628fba47bc8f66cefeca2aad515746acf0a2
Udayashankar MS <udayashankar.ms@global-infonet.com>
2022-05-18 22:57:41 +0530

    Sixth Commit

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
 
 # on the terminal type: curl http://127.0.0.1:5000/
 # returns hello world when we use GET.
 # returns the data that we send when we use POST.
 @app.route('/extract', methods=['POST'])
 def home():
-    data = json.loads(request.data)
-    model = data["model"]
-    document = data["doc"]
-
-    trainer = Trainer()
-    entities = trainer.extract_entities(document, model)
-    filtered_entities = Trainer.convert_result(entities.ents)
-    return jsonify(filtered_entities)
+    exception = "";
+    try:
+        data = json.loads(request.data)
+        model = data["model"]
+        document = data["doc"]
+        exception = "Got data"
+        trainer = Trainer()
+        exception = "Got data 1"
+        entities = trainer.extract_entities(document, model)
+        exception = "Got data 2"
+        filtered_entities = Trainer.convert_result(entities.ents)
+        exception = "Got data 3"
+        return jsonify(filtered_entities)
+    except:
+        return exception
